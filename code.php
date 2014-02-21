1bba6a2ad58879da0e1175512c7a589653af636e
jr vazquez <kwikteck@gmail.com>
2014-02-20 19:23:39 -0600

    Added variables to Code.php
    added a few variables to testing

--- code.php
+++ code.php
@@ -1,2 +1,23 @@
 <h1><?php echo "Shirts 4 Mike"; ?></h1>
 <p>The current time is <?php echo date("H:i:s a"); ?>.</p>
+<?php 
+$year = date('y'); 
+echo $year; 
+?>
+
+<?php 
+$productName = "Mike the Frog Shirt, Orange";
+$productPrice = 20;
+?>
+<ul>
+<li>
+	<?php echo $productName; ?> - Price: <?php echo $productPrice; ?>
+</li>
+<?php
+$productName = "Logo Shirt, Blue";
+$productPrice = 25;
+?>
+<li>
+	<?php echo $productName; ?> - Price: <?php echo $productPrice; ?>
+</li>
+</ul>
