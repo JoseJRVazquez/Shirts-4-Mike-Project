1b423e3137083b78c171b143964aaf8962449dd0
jr vazquez <kwikteck@gmail.com>
2014-02-20 19:50:37 -0600

    Added Conditionals
    Added some sample conditionals to code page: Branch ends here

--- code.php
+++ code.php
@@ -21,3 +21,27 @@ $productPrice = 25;
 	<?php echo $productName; ?> - Price: <?php echo $productPrice; ?>
 </li>
 </ul>
+
+
+<?php 
+$today = "Saturday";
+if ($today == "Monday") {
+	echo "Welcome back to work!";
+}
+
+<?php
+$hour = date('G');
+if ($hour < 12) {
+	echo "Good Morning!";
+}
+if ($hour >=12 and $hour < 17) {
+	echo "Good Afternoon!";
+}
+if ($hour >= 17 and $hour < 20) {
+	echo "Good Evening!";
+}
+if ($hour >= 20){
+	echo "Good night!";
+}
+
+
