96a2ad9763fa7f9decb63437d476c9d93fcddcff
jr vazquez <kwikteck@gmail.com>
2014-02-20 22:14:45 -0600

    Minor changes

--- contact.php
+++ contact.php
@@ -4,7 +4,23 @@ $section = "contact";
 include('inc/header.php'); ?>
 
 <div class="section page">
-	<h1>Contact</h1>
+
+	<div class="wrapper">
+
+		<h1>Contact</h1>
+
+		<p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
+
+		<form method="post">
+
+			<label for="name">Name</label><input type="text" name="name" id="name"> 
+
+		</form>
+
 </div>
+	
+</div>
+
+
 
 <?php include('inc/footer.php'); ?>
