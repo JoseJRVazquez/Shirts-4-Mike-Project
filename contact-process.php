4b44680b80bf554f40a36ee870122c368abe52c3
jr vazquez <kwikteck@gmail.com>
2014-02-20 23:03:38 -0600

    Added contact-thanks form
    Added variables and contact thanks page for submission: email action
    still missing.

--- contact-process.php
+++ contact-process.php
@@ -3,8 +3,13 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-echo $name;
-echo $email;
-echo $message;
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message;
+
+// TODO: Send Email
+
+header("Location: contact-thanks.php");
 
 ?>
