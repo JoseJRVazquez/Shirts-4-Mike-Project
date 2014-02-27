613500c194a3d073fdb14d6ba8a9b9be257aff30
jr vazquez <kwikteck@gmail.com>
2014-02-27 12:12:24 -0600

    added Arrays file

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php 
+
+$flavors = array("Vanilla", "Chocolate", "Strawberry")
+
+
+?><pre>
+<?php echo "We have " . count($flavors) . " flavors available. Here's a list:"; 
+
+foreach ($flavors)
+
+?>
+
+</pre>
