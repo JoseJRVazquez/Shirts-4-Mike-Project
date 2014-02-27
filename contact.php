37ed99d3d95dd27944bc5193a32bcb6082e8dc1b
jr vazquez <kwikteck@gmail.com>
2014-02-27 11:10:13 -0600

    Deleted contact-process
    deleted contact process and merged file into contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,22 @@
+<?php
+
+// This code establishes the variables of the email form
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+//This code establishes the content of the email in a readable format
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message;
+
+// Still to do is the Send Email protocol
+
+//This forwards the page to the thank you page
+header("Location: contact-thanks.php");
+
+?>
 <?php 
 $pageTitle = "Contact Mike";
 $section = "contact";
